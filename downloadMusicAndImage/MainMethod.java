b23edfb96f999c32ece73a9a8dcdc27aafa2ae80
CarrBeat (Volodin Mikhail) <77549980+CarrBeat@users.noreply.github.com>
2021-11-26 11:25:47 +0300

    Add files via upload

---
+++ downloadMusicAndImage/MainMethod.java
@@ -0,0 +1,9 @@
+package downloadMusicAndImage;
+
+import java.lang.annotation.*;
+
+@Target(value = ElementType.METHOD)
+@Documented
+@Retention(value = RetentionPolicy.SOURCE)
+public @interface MainMethod { // аннотация обозначения метода
+}
